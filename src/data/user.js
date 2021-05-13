f2b123ec1870d44b1b4e88d0f41fe1816825438a
WalukaUI <cwaluka@yahoo.com>
2021-05-12 20:37:52 -0700

    5 passings

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,8 @@
-const username = "Liza";
-const city = "New York";
+export const username = "Liza";
+export const city = "New York";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+export const image = "https://i.imgur.com/mV8PQxj.gif"
 
+
+export default image
 // export all three variables as named exports
